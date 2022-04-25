61a3033f701bee7c5fe97a6a8f28f77cf4b1ea36
lehoanganh298 <lehoanganh29896@gmail.com>
2022-04-25 19:38:52 +0700

    Refactor Fix minor bugs

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,63 @@ import * as vscode from 'vscode';
 import {createReadStream} from 'fs';
 import {createInterface} from 'readline';
 
+const jsonlScheme = 'jsonl';
+let lineIndexDict = Object();	// Store current line index of previewed json files
 let lineIdxStatusBarItem: vscode.StatusBarItem;
 
-async function processLineByLine(uri: vscode.Uri, lineIdx: number): Promise<[string,number]> {
+// A custom content provider for jsonl file
+class JsonlContentProvider implements vscode.TextDocumentContentProvider {
+
+	onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+	onDidChange = this.onDidChangeEmitter.event;
+
+	async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+		let lineIdx = lineIndexDict[uri.path];
+		if (lineIdx === undefined) {
+			lineIdx=1;
+			lineIndexDict[uri.path]=lineIdx;
+		}
+		const res = await readFileAtLine(uri,lineIdx);
+		lineIndexDict[uri.path] = res[1]; // handle when line index invalid
+		updateLineIdxStatusBarItem();
+		
+		const lineFormated = JSON.stringify(JSON.parse(res[0]), null, 2);
+		return lineFormated;
+	}
+};
+
+const jsonlProvider = new JsonlContentProvider();
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(jsonlScheme, jsonlProvider));
+
+	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.preview', openPreviewHandler));
+	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.next-line', nextLineHandler));
+	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.previous-line', previousLineHandler));
+	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.go-to-line',goToLine));
+
+	lineIdxStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100000);
+	context.subscriptions.push(lineIdxStatusBarItem);
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateLineIdxStatusBarItem));
+	updateLineIdxStatusBarItem();
+}
+
+// Read a file content at specified line index
+// If line index <=0, return first line
+// If line index exceed file's line count, return last line
+// Input: 	- file's uri
+// 			- line index
+// Output: 	- line's content
+// 			- returned line index
+async function readFileAtLine(uri: vscode.Uri, lineIdx: number): Promise<[string,number]> {
+	if (lineIdx<=0) {
+		lineIdx = 1;
+	}
+
 	const fileStream = createReadStream(uri.path.replace('(preview)','').trimEnd());
   
 	const rl = createInterface({
@@ -28,139 +82,95 @@ async function processLineByLine(uri: vscode.Uri, lineIdx: number): Promise<[str
 	return [line, idx];
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  
-	const jsonlScheme = 'jsonl';
-	let lineIndexDict = Object();
 
-	const myProvider = new class implements vscode.TextDocumentContentProvider {
-
-		// emitter and its event
-		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-		onDidChange = this.onDidChangeEmitter.event;
-
-		async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-			let lineIdx = lineIndexDict[uri.path];
-			if (lineIdx === undefined) {
-				lineIdx=1;
-				lineIndexDict[uri.path]=lineIdx;
-			}
-			const res = await processLineByLine(uri,lineIdx);
-			lineIndexDict[uri.path] = res[1]; // handle when line index exceed file line count
-			updateStatusBarItem();
-			
-			const lineFormated = JSON.stringify(JSON.parse(res[0]), null, 2);
-			return lineFormated;
-		}
-	};
-
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(jsonlScheme, myProvider));
-
-
-	const openPreviewHandler = async (arg: any) => {
-		let uri = arg;
-        if (!(uri instanceof vscode.Uri)) {
-			if (vscode.window.activeTextEditor) {
-                uri = vscode.window.activeTextEditor.document.uri;
-            } else {
-                vscode.window.showInformationMessage("Open a JSON Line file first to show a preview.");
-                return;
-            }
-        }
-		
-		// Change uri-scheme to "jsonl"
-		// TODO: Add line index to title
-		const jsonlUri = vscode.Uri.parse('jsonl:' + uri.path + ' (preview)');
-
-		const document = await vscode.workspace.openTextDocument(jsonlUri);
-		await vscode.window.showTextDocument(document);
-		
-		await vscode.languages.setTextDocumentLanguage(document, "json");
-		// await vscode.commands.executeCommand('editor.action.formatDocument');
-	};
-
-	const nextLineHandler = async () => {
-		if (!vscode.window.activeTextEditor) {
-			return; // no editor
-		}
-		const { document } = vscode.window.activeTextEditor;
-		if (document.uri.scheme !== jsonlScheme) {
-			return; // not my scheme
-		}
-		lineIndexDict[document.uri.path]+=1;
-		myProvider.onDidChangeEmitter.fire(document.uri);
-	};
-	
-	const previousLineHandler = async () => {
-		if (!vscode.window.activeTextEditor) {
-			return; // no editor
-		}
-		const { document } = vscode.window.activeTextEditor;
-		if (document.uri.scheme !== jsonlScheme) {
-			return; // not my scheme
-		}
-		lineIndexDict[document.uri.path]-=1;
-
-		if (lineIndexDict[document.uri.path]<=0) {
-			lineIndexDict[document.uri.path]=1;
-		}
-		else {
-			myProvider.onDidChangeEmitter.fire(document.uri);
-		}
-	};
-
-	const goToLine = async () => {
-		if (!vscode.window.activeTextEditor) {
-			return; // no editor
-		}
-		const { document } = vscode.window.activeTextEditor;
-		if (document.uri.scheme !== jsonlScheme) {
-			return; // not my scheme
-		}
-
-		let lineIdx = null;
-		while (lineIdx === null || isNaN(lineIdx)){
-			let lineIdxStr = await vscode.window.showInputBox(
-				{ prompt: 'Type a line number to preview Json object at that line.' });
-			if (lineIdxStr === undefined) {
-				break;
-			}
-
-			lineIdx = parseInt(lineIdxStr);
-		}
-		if (lineIdx !== null){
-			lineIndexDict[document.uri.path] = lineIdx;
-			myProvider.onDidChangeEmitter.fire(document.uri);
-		}
-	};
-
-	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.preview', openPreviewHandler));
-	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.next-line', nextLineHandler));
-	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.go-to-line',goToLine));
-	
-	context.subscriptions.push(vscode.commands.registerCommand('json-line-viewer.previous-line', previousLineHandler));
-
-	lineIdxStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100000);
-	context.subscriptions.push(lineIdxStatusBarItem);
-	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	updateStatusBarItem();
-
-	function updateStatusBarItem(): void {
-		if (!vscode.window.activeTextEditor) {
-			lineIdxStatusBarItem.hide(); // no editor
+const openPreviewHandler = async (arg: any) => {
+	let uri = arg;
+	if (!(uri instanceof vscode.Uri)) {
+		if (vscode.window.activeTextEditor) {
+			uri = vscode.window.activeTextEditor.document.uri;
+		} else {
+			vscode.window.showInformationMessage("Open a JSON Line file first to show a preview.");
 			return;
 		}
-		const { document } = vscode.window.activeTextEditor;
-		if (document.uri.scheme !== jsonlScheme) {
-			lineIdxStatusBarItem.hide();
-			return; 
-		}
-		lineIdxStatusBarItem.text = `JSONL at line: ${lineIndexDict[document.uri.path]}`;
-		lineIdxStatusBarItem.show();
 	}
+	
+	// Change uri-scheme to "jsonl"
+	const jsonlUri = vscode.Uri.parse('jsonl:' + uri.path + ' (preview)');
+
+	const document = await vscode.workspace.openTextDocument(jsonlUri);
+	await vscode.window.showTextDocument(document);
+	
+	await vscode.languages.setTextDocumentLanguage(document, "json");
+};
+
+
+const nextLineHandler = async () => {
+	if (!vscode.window.activeTextEditor) {
+		return; // no editor
+	}
+	const { document } = vscode.window.activeTextEditor;
+	if (document.uri.scheme !== jsonlScheme) {
+		return; // not my scheme
+	}
+	lineIndexDict[document.uri.path]+=1;
+	jsonlProvider.onDidChangeEmitter.fire(document.uri);
+};
+
+
+const previousLineHandler = async () => {
+	if (!vscode.window.activeTextEditor) {
+		return; // no editor
+	}
+	const { document } = vscode.window.activeTextEditor;
+	if (document.uri.scheme !== jsonlScheme) {
+		return; // not my scheme
+	}
+	lineIndexDict[document.uri.path]-=1;
+
+	jsonlProvider.onDidChangeEmitter.fire(document.uri);
+};
+
+
+const goToLine = async () => {
+	if (!vscode.window.activeTextEditor) {
+		return; // no editor
+	}
+	const { document } = vscode.window.activeTextEditor;
+	if (document.uri.scheme !== jsonlScheme) {
+		return; // not my scheme
+	}
+
+	let lineIdx = null;
+	while (lineIdx === null || isNaN(lineIdx)){
+		let lineIdxStr = await vscode.window.showInputBox(
+			{ prompt: 'Type a line number to preview Json object at that line.' });
+		if (lineIdxStr === undefined) {
+			break;
+		}
+
+		lineIdx = parseInt(lineIdxStr);
+	}
+	if (lineIdx !== null){
+		lineIndexDict[document.uri.path] = lineIdx;
+		jsonlProvider.onDidChangeEmitter.fire(document.uri);
+	}
+};
+
+
+function updateLineIdxStatusBarItem(): void {
+	if (!vscode.window.activeTextEditor) {
+		lineIdxStatusBarItem.hide(); // no editor
+		return;
+	}
+	const { document } = vscode.window.activeTextEditor;
+	if (document.uri.scheme !== jsonlScheme) {
+		lineIdxStatusBarItem.hide();
+		return; 
+	}
+	lineIdxStatusBarItem.text = `JSONL at line: ${lineIndexDict[document.uri.path]}`;
+	lineIdxStatusBarItem.show();
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
